cef081b85db1533d3ed40c289b59312a75521ab9
IlgarZaman <ilqarzamanov20@gmail.com>
2023-12-28 14:05:45 +0300

    edit dark mode btn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,14 @@ import "./App.scss";
 import Header from "./layouts/header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import { useEffect } from "react";
+import { setInitialDarkMode } from "./redux/slice/darkModeSlice";
+import store from "./redux/store.js";
+
 function App() {
+  useEffect(() => {
+    store.dispatch(setInitialDarkMode());
+  }, []);
   return (
     <>
       <Header />
